0194acd40c7a9dd902bb7ae2cf946ebf71ac1e68
laalipop <preicpodhya@mail.ru>
2021-01-12 18:37:49 +0300

    changed the tests

--- src/js/__tests__/basic.test.js
+++ src/js/__tests__/basic.test.js
@@ -15,3 +15,19 @@ test('test sortByHealth', () => {
 
   expect(sortByHealth(list)).toEqual(listTwo);
 });
+
+test('test sortByHealth', () => {
+  const list = [
+    { name: 'мечник', health: 10 },
+    { name: 'маг', health: 100 },
+    { name: 'лучник', health: 80 },
+  ];
+
+  const listTwo = [
+    { name: 'маг', health: 100 },
+    { name: 'лучник', health: 80 },
+    { name: 'мечник', health: 10 },
+  ];
+
+  expect(sortByHealth(list)).not.toBe(listTwo);
+});
